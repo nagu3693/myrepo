abd87c13c651486ae699968fcee13277ca9f48e2
nagu3693 <nagaraju.gokaraju@gmail.com>
2020-03-03 19:39:46 +0000

    addded variable script

---
+++ python_scripts/varaible.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3.6
+first_name = "nagaraju"
+last_name = "gokaraju"
+age = 35
+birth_date = "March 4th"
+print("My name is",first_name,last_name,".")
+print("I was born on",birth_date,", and I'm",age,"years old.")
