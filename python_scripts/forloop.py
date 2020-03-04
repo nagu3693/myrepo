f4ebc750ae4e0af57c16e531b9ff7470241b55b9
nagu3693 <nagaraju.gokaraju@gmail.com>
2020-03-04 00:29:31 +0000

    forloop doc and script

---
+++ python_scripts/forloop.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3.6
+colors=['red','blue','green','yellow']
+for color in colors:
+    if color == 'red':
+        continue
+    elif color == 'blue':
+     break
+    print(color)
