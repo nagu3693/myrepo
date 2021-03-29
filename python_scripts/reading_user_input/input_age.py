7a35587d0e7dd3781437168d0334ca06bd830904
nagu3693 <nagaraju.gokaraju@gmail.com>
2021-03-29 20:25:29 +0000

    input birthday and check if the age is half

---
+++ python_scripts/reading_user_input/input_age.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3.6
+name = input("What is your  name ? ")
+birthdat = input("What is your birthdate? ")
+age = int(input("How old are you? "))
+print(f"{name} was born on {birthdat}")
+print(f"Half of your age is {age / 2}")
