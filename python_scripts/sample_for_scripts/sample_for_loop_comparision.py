9c0b08180ef6b236ac35c91daad968c4d9598c23
nagu3693 <nagaraju.gokaraju@gmail.com>
2021-03-29 13:40:02 +0000

    adding python loop comparision

---
+++ python_scripts/sample_for_scripts/sample_for_loop_comparision.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3.6
+colors = ['blu', 'gre', 'red', 'purple']
+for color in colors:
+        if color == 'blue':
+            continue
+        elif color == 'gre':
+            break
+        print(color)
+
