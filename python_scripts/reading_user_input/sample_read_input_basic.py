7f29593c51c9c39e9e66c5dacfa7355ca5bde131
nagu3693 <nagaraju.gokaraju@gmail.com>
2021-03-29 19:39:06 +0000

    Basic input script to print first and last name

---
+++ python_scripts/reading_user_input/sample_read_input_basic.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3.6
+firstname = input("What is your first name ? ")
+lastname = input("What is your last name ?")
+print(f" first name is  {firstname} lastname is {lastname}")
