544a398cf28f2750f5d8208eb9481439c2787317
nagu3693 <nagaraju.gokaraju@gmail.com>
2021-04-01 17:22:07 +0000

    create a stop watch application

--- python_scripts/using_standard_library/importing_time_module_basic.py
+++ python_scripts/using_standard_library/importing_time_module_basic.py
@@ -1,5 +1,10 @@
 #!/usr/local/bin/python3.6
-import time
-start_time = time.localtime()
-print(f" Timer  started at {time.strftime('%X',start_time)}")
-
+from time import localtime,strftime,mktime
+start_time = localtime()
+print(f" Timer  started at {strftime('%X',start_time)}")
+# Wait for user to stop timer
+input("Press enter key to stop timer")
+stop_time = localtime()
+difference = mktime(stop_time) - mktime(start_time)
+print(f" Timer  started at {strftime('%X',stop_time)}")
+print(f" Total time: {difference}")
