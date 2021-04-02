760577f8a545caca39d4055e44eb3b06c3e05825
nagu3693 <nagaraju.gokaraju@gmail.com>
2021-04-02 20:54:34 +0000

    positional parmeter

---
+++ python_scripts/Parsing_Command_Line_Parameters/param_echo.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3.6
+import sys
+#print(f"Posistional argument {sys.argv[1:]}")
+#print(f"First argument: {sys.argv[1]}")
+#print(f"Second argument: {sys.argv[2]}")
+print(f"Posistional parmeter: {sys.argv[1:]}")
+print(f"First parmeter: {sys.argv[1]}")
+print(f"Second parameter: {sys.argv[3]}")
